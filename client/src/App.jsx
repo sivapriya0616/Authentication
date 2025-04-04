73cffb3f7d9a810459f42e73ce93001e3a6b7f98
priya <sivapragasampriya01@gmail.com>
2025-04-04 16:45:10 +0530

    Add authentication check and logout functionality; update navigation links based on authentication state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,37 @@
-import React from "react";
-// import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-
-// import { useState ,useEffect} from "react";
 import "./pages/Style.css";
 
 const App = () => {
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    axios.get("http://localhost:5001", { withCredentials: true })
+      .then(res => setIsAuthenticated(res.data.valid))
+      .catch(err => console.error(err));
+  }, []);
+
+  const handleLogout = () => {
+    axios.post("http://localhost:5001/Logout", {}, { withCredentials: true })
+      .then(() => {
+        setIsAuthenticated(false);
+        window.location.href = "/login";
+      })
+      .catch(err => console.error("Logout failed:", err));
+  };
+
   return (
-  
-       <Router>
-      
+    <Router>
       <div className="App">
         <nav>
           <ul>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/login">Logout</Link></li>
+            {!isAuthenticated && <li><Link to="/login">Login</Link></li>}
+            {!isAuthenticated && <li><Link to="/register">Register</Link></li>}
+            {/* {isAuthenticated && <li><button onClick={handleLogout}>Logout</button></li>} */}
           </ul>
         </nav>
 
@@ -28,15 +39,10 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Home />} />
-
         </Routes>
-        
       </div>
     </Router>
-    
-  )
-}
+  );
+};
 
 export default App;
-
-             
